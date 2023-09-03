f05b822a6573ee3b907e9d9906ea51d47b904d69
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-09-04 00:07:56 +0300

    Adding Container and testing if the scss setup works

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom';
-import styles from './Navbar.scss';
-
+import styles from './Navbar.module.scss';
+import { Container } from '../Container/Container';
 
 export const Navbar = () => {
     return (
-        <div>
-            <div className="logo">
-
-            </div>
-            <nav>
-                {/* <Link to="/">Home</Link>     */}
-            </nav>
-        </div>
+        <header className={styles.header}>
+            <Container>
+                <div className={styles.navContainer}>
+                    <div className="logo">
+                        <Link to="/">Logo</Link>
+                    </div>
+                    <nav>
+                        <Link to="/store">Store</Link>
+                        <div>Cart</div>
+                    </nav>
+                </div>
+            </Container>
+        </header>
     );
 };
