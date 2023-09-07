f4240cb2465d842db81e717a66f50a2381fd12ee
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-09-07 21:17:47 +0300

    Adding pictures

--- src/components/Showcase/Showcase.tsx
+++ src/components/Showcase/Showcase.tsx
@@ -4,17 +4,14 @@ import billionairesData from 'src/data/billionaires.json';
 import styles from './Showcase.module.scss';
 import { Card } from './Card';
 
-
-
 export const Showcase = () => {
     return (
         <section>
             <Container>
+                <h1 className={styles.title}>Pick a Billionaire. Raid Their Wallet. Live Large!</h1>
                 <div className={styles.cards}>
                     {billionairesData.map((billionaire) => (
-                        <Card 
-                            key={billionaire.id} {...billionaire} 
-                        />
+                        <Card key={billionaire.id} {...billionaire} />
                     ))}
                 </div>
             </Container>
