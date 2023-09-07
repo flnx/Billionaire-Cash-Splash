cdf6dd774341886e345d3a721fe2ba4c11247630
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-09-07 23:40:22 +0300

    adding styles to products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,13 +1,11 @@
 import productsData from 'src/data/products.json';
 import { Product } from './Product';
 
+import styles from './Products.module.scss';
+
 export const Products = () => {
     return (
-        <div
-            style={{
-                display: 'flex',
-            }}
-        >
+        <div className={styles.products}>
             {productsData.map((product) => (
                 <Product key={product.id} {...product} />
             ))}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Container } from 'src/components/Container/Container';
+import { Products } from 'src/components/Products/Products';
 import { Showcase } from 'src/components/Showcase/Showcase';
 
 export const Home = () => {
     return (
         <Container>
             <Showcase />
+            <Products />
         </Container>
     );
 };
