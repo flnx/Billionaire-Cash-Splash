c2342559061c8a76e11fe5df9d4f20458a57e399
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-10-19 12:51:21 +0300

    Update ShoppingCartReducer.ts

--- src/components/utils/ShoppingCartReducer.ts
+++ src/components/utils/ShoppingCartReducer.ts
@@ -1,19 +1,41 @@
-type ActionType = {
-    INCREASE: 'INCREASE';
-    DECREASE: 'DECREASE';
-    QUANTITY: 'QUANTITY';
-    REMOVE: 'REMOVE';
-};
+// type ActionType = {
+//     INCREASE: 'INCREASE';
+//     DECREASE: 'DECREASE';
+//     QUANTITY: 'QUANTITY';
+//     REMOVE: 'REMOVE';
+// };
+
+// type CartItemType = {
+//     id: number;
+//     name:  string;
+//     price: number;
+//     imageUrls: string[];
+// };
+
+// type ReducerAction = {
+//     type: ActionType;
+//     payload?: CartItemType;
+// };
+
+// type StateType = {
+//     cart: CartItemType[];
+// };
+
+// const shoppingCartReducer = (state: StateType, action: ReducerAction) => {
+
+// };
+
+type ActionType = 'INCREASE' | 'DECREASE' | 'QUANTITY' | 'REMOVE';
 
 type CartItemType = {
     id: number;
-    name:  string;
+    name: string;
     price: number;
     imageUrls: string[];
 };
 
-type ReducerAction = {
-    type: string;
+type ReducerActions = {
+    type: ActionType;
     payload?: CartItemType;
 };
 
@@ -21,6 +43,6 @@ type StateType = {
     cart: CartItemType[];
 };
 
-// const shoppingCartReducer = (state, action) => {
-
-// };
+const shoppingCartReducer = (state: StateType, action: ReducerActions) => {
+    
+};
