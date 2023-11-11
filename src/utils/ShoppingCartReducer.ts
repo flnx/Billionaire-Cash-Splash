722768689db3eec8a7a03f1bd1135fd964a5bf27
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-11-11 20:00:12 +0200

    Adding Context

---
+++ src/utils/ShoppingCartReducer.ts
@@ -0,0 +1,44 @@
+type ActionType = 'INCREASE' | 'DECREASE' | 'QUANTITY' | 'REMOVE';
+
+type CartItemType = {
+    id: number;
+    name: string;
+    price: number;
+    imageUrls: string[];
+    qty: number;
+};
+
+export type ReducerActions = {
+    type: ActionType;
+    payload?: CartItemType;
+};
+
+export type StateType = {
+    cart: CartItemType[];
+};
+
+export const shoppingCartReducer = (state: StateType, action: ReducerActions) => {
+    switch (action.type) {
+        case 'INCREASE': {
+            if (!action.payload) {
+                throw new Error(
+                    'action.payload is missing in INCREASE section'
+                );
+            }
+
+            const { id } = action.payload;
+
+            const updatedCart = state.cart.map((item) =>
+                item.id === id ? { ...item, qty: (item.qty || 0) + 1 } : item
+            );
+
+            return {
+                ...state,
+                cart: updatedCart,
+            };
+        }
+
+        default:
+            return state;
+    }
+};
