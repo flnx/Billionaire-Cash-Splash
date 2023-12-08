568c9bcf81eea6bf076bf4358c0bdd9936729e0b
Kaloyan Georgiev <definitelywebdev@yahoo.com>
2023-12-08 16:17:15 +0200

    changing inventory item text size

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,11 +1,9 @@
 import productsData from 'src/data/products.json';
 import { Product } from './Product';
-import { useBillionaire } from 'src/context/BillionaireContext';
 
 import styles from './Products.module.scss';
 
 export const Products = () => {
-    // const { isBillionaireSelected } = useBillionaire();
 
     return (
         <div className={styles.products}>
